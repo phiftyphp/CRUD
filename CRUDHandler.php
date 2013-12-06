5360c2e21f8749e9b89e1c8f6301eb7b39d1e6fa
c9s <cornelius.howl@gmail.com>
2013-12-06 17:38:27 +0800

    Save bundle object from CRUDHandler.

--- CRUDHandler.php
+++ CRUDHandler.php
@@ -244,6 +244,7 @@ abstract class CRUDHandler extends BaseCRUDHandler
 
         // XXX: currently we use FooBundle\FooBundle as the main bundle class.
         $bundleClass = "$ns\\$ns";
+        $this->bundle = $this->vars['Bundle'] = $bundleClass::getInstance();
 
 
         // anyway, we have the model classname, and the namespace, 
