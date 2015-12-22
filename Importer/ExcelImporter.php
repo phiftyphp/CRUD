6389ad6ce9020cf42ddcaf910c189a9a6b642394
c9s <yoanlin93@gmail.com>
2015-12-22 19:36:06 +0800

    Implement ExcelImporter

--- Importer/ExcelImporter.php
+++ Importer/ExcelImporter.php
@@ -1,12 +1,12 @@
 <?php
-namespace CRUD;
+namespace CRUD\Importer;
 use PHPExcel_IOFactory;
 use LazyRecord\BaseModel;
 use LazyRecord\Schema\DeclareSchema;
-
+use PHPExcel;
 
 /**
- * Generates data preview from uploaded excel file.
+ * Handle data import from excel file
  */
 class ExcelImporter
 {
@@ -14,27 +14,91 @@ class ExcelImporter
 
     protected $schema;
 
-    protected $filepath;
+    protected $importFields;
 
-    public function __construct(BaseModel $model, $filepath)
+    public function __construct(BaseModel $model, array $importFields = null)
     {
         $this->model = $model;
         $this->schema = $model->getSchema();
-        $this->excelPath = $filepath;
+        $this->importFields = $importFields ?: $this->schema->getColumnNames();
     }
 
-    public function import()
+    public function createSampleExcel($generateSampleContent = false)
     {
+        $excel = new PHPExcel;
+        /*
+        $excel->getProperties()->setCreator("Maarten Balliauw")
+            ->setLastModifiedBy("Maarten Balliauw")
+            ->setTitle("Office 2007 XLSX Test Document")
+            ->setSubject("Office 2007 XLSX Test Document")
+            ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+            ->setKeywords("office 2007 openxml php")
+            ->setCategory("Test result file")
+            ;
+        */
+        $columnKeys = range('A', 'Z');
+        $columnKeys = array_splice($columnKeys, 0, count($this->importFields));
 
+        $sheet = $excel->setActiveSheetIndex(0);
+
+        // outout headers
+        foreach ($columnKeys as $index => $key) {
+            $field = $this->importFields[$index];
+            $sheet->setCellValue($key . '1', $this->schema->getColumn($field)->getLabel());
+        }
+
+        // generate sample data from placeholder
+        if ($generateSampleContent) {
+            for ($i = 2 ; $i < 5 ; $i++) {
+                foreach ($columnKeys as $index => $key) {
+                    $field = $this->importFields[$index];
+                    $sheet->setCellValue($key . $i, $this->schema->getColumn($field)->placeholder ?: 'sample');
+                }
+            }
+        }
+
+        return $excel;
+    }
+
+    public function import($filepath)
+    {
+        $excel = PHPExcel_IOFactory::load($filepath);
+        $worksheet   = $excel->getActiveSheet();
+        $sheetTitle  = $worksheet->getTitle();
+        $rowIterator = $worksheet->getRowIterator();
+
+        $headers = array();
+        $cellIterator = $rowIterator->current()->getCellIterator();
+        $cellIterator->setIterateOnlyExistingCells(false); // Loop all cells, even if it is not set
+        foreach ($cellIterator as $cell) {
+            $headers[] = $cell->getCalculatedValue();
+        }
+
+        $rows = [];
+        $rowIterator->next(); // skip the header row
+        while ($maxRows-- && $rowIterator->valid()) {
+            $row = $rowIterator->current();
+
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(false); // Loop all cells, even if it is not set
+
+            $rowArray = [];
+            foreach ($cellIterator as $cell) {
+                $rowArray[] = trim($cell->getCalculatedValue());
+            }
+            $rowIterator->next();
+            $rows[] = $rowArray;
+        }
+        return $rows;
     }
 
     /**
      * @return array [ 'headers' => ..., 'rows' => .... ]
      */
-    public function getPreview($maxRows = 5)
+    public function preview($filepath, $maxRows = 5)
     {
         $return = [];
-        $excel = PHPExcel_IOFactory::load($this->filepath);
+        $excel = PHPExcel_IOFactory::load($filepath);
         $worksheet   = $excel->getActiveSheet();
         $sheetTitle  = $worksheet->getTitle();
         $rowIterator = $worksheet->getRowIterator();
