26eca9490457499bba7fad388fcd05c7987f2daa
c9s <yoanlin93@gmail.com>
2015-12-27 00:34:41 +0800

    Importer improvements - check primary key

--- Importer/ExcelImporter.php
+++ Importer/ExcelImporter.php
@@ -4,6 +4,7 @@ use PHPExcel_IOFactory;
 use LazyRecord\BaseModel;
 use LazyRecord\Schema\DeclareSchema;
 use PHPExcel;
+use PHPExcel_Cell;
 use Exception;
 
 /**
@@ -11,9 +12,15 @@ use Exception;
  */
 class ExcelImporter
 {
+    const IDENTIFY_BY_PRIMARY_KEY = 1;
+
+    const IDENTIFY_BY_COLUMNS = 2;
+
     protected $schema;
 
     protected $importFields;
+
+    protected $skipBy = self::IDENTIFY_BY_PRIMARY_KEY;
 
     public function __construct($schema, array $importFields = null)
     {
@@ -67,10 +74,14 @@ class ExcelImporter
 
         $headers = array();
         $cellIterator = $rowIterator->current()->getCellIterator();
-        $cellIterator->setIterateOnlyExistingCells(true); // Loop all cells, even if it is not set
+        $cellIterator->setIterateOnlyExistingCells(false); // Loop all cells, even if it is not set
         foreach ($cellIterator as $cell) {
             $headers[] = $cell->getCalculatedValue();
         }
+
+
+        $imported = 0;
+        $skipped = 0;
 
         $records = [];
         $rowIterator->next(); // skip the header row
@@ -80,7 +91,8 @@ class ExcelImporter
             $cellIterator->setIterateOnlyExistingCells(false); // Loop all cells, even if it is not set
 
             $data = [];
-            foreach ($cellIterator as $index => $cell) {
+            foreach ($cellIterator as $cell) {
+                $index = PHPExcel_Cell::columnIndexFromString($cell->getColumn()) - 1;
                 $key = $headers[$index];
                 if ($column = $this->schema->getColumn($key)) {
                     $text = trim($cell->getCalculatedValue());
@@ -96,15 +108,27 @@ class ExcelImporter
             }
 
             $record = $this->schema->newModel();
+
+            if ($this->skipBy == self::IDENTIFY_BY_PRIMARY_KEY) {
+                if (isset($data[$this->schema->primaryKey])) {
+                    $ret = $record->find($data[$this->schema->primaryKey]);
+                    if ($ret->success) {
+                        $skipped++;
+                        $rowIterator->next();
+                        continue;
+                    }
+                }
+            }
+
             $ret = $record->create($data);
             if ($ret->error) {
                 throw new Exception('Import error: ' . $ret->message);
             }
-
+            $imported++;
             $rowIterator->next();
             $records[] = $record;
         }
-        return $records;
+        return [$imported, $skipped, $records];
     }
 
     /**
