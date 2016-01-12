183fdd6dec42cf7557818341431254e3ec01ed14
c9s <yoanlin93@gmail.com>
2016-01-12 16:08:13 +0800

    Add getPermissionConfig method to return all the permission items

--- CRUDHandler.php
+++ CRUDHandler.php
@@ -1549,4 +1549,24 @@ abstract class CRUDHandler extends BaseCRUDHandler
     }
 
 
+    /**
+     * Return the config of the current permission items
+     */
+    public function getPermissionConfig()
+    {
+        return [
+            'canCreate'           => $this->canCreate,
+            'canUpdate'           => $this->canUpdate,
+            'canDelete'           => $this->canDelete,
+            'canExport'           => $this->canExport,
+            'canImport'           => $this->canImport,
+            'canBulkEdit'         => $this->canBulkEdit,
+            'canBulkCopy'         => $this->canBulkCopy,
+            'canBulkDelete'       => $this->canBulkDelete,
+            'canEditInNewWindow'  => $this->canEditInNewWindow,
+        ];
+    }
+
+
+
 }
