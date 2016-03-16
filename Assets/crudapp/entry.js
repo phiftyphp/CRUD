8469b74355210230044d8b2405e77170ec4e40a5
c9s <yoanlin93@gmail.com>
2016-03-16 23:56:51 +0800

    Add entry.js

---
+++ Assets/crudapp/entry.js
@@ -0,0 +1,9 @@
+import SetPasswordControl from 'crudapp/components/SetPasswordControl';
+import DateRangeControl from 'crudapp/components/DateRangeControl';
+import SingleDayControl from 'crudapp/components/SingleDayControl';
+import BootstrapFormHighlight from "crudapp/components/BootstrapFormHighlight";
+
+window.BootstrapFormHighlight = BootstrapFormHighlight;
+window.SetPasswordControl = SetPasswordControl;
+window.DateRangeControl = DateRangeControl;
+window.SingleDayControl = SingleDayControl;
