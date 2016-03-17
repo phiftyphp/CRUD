45f0d10284b67a3d3f9f06a929c2c2d36a695968
c9s <yoanlin93@gmail.com>
2016-03-17 14:01:52 +0800

    Add css styles for crudrelapp

---
+++ Assets/crud/webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var phifty = require('phifty');
+var webpack = require("phifty/node_modules/webpack");
+var excludePaths = phifty.webpackExcludePaths();
+var aliases = phifty.assetAliases();
+
+module.exports = {
+  entry: __dirname + '/entry.js',
+  output: { path: __dirname, filename: './bundle.js' },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      loaders: ['babel-loader'],
+      exclude: excludePaths
+    }]
+  },
+  externals: {
+    // don't bundle the 'react' npm package with our bundle.js
+    // but get it from a global 'React' variable
+    'react': 'React'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    fallback: [ path.join(__dirname, "node_modules"), phifty.moduleDirectory],
+    alias: aliases
+  },
+
+  resolveLoader: {
+    fallback: [ path.join(__dirname, "node_modules"), phifty.moduleDirectory]
+  }
+};
