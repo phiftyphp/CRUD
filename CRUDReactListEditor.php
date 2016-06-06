7f2cc75d9d3c0952f2b81f393a44f50927694cdc
c9s <yoanlin93@gmail.com>
2016-06-06 14:19:56 +0900

    update CRUDReactListEditor trait

--- CRUDReactListEditor.php
+++ CRUDReactListEditor.php
@@ -11,5 +11,52 @@ trait CRUDReactListEditor extends CRUDReactListApp {
      * @var string The react application name
      */
     protected $reactListApp = 'CRUDReactListEditor';
+
+    /**
+     * Export config array for building react list app
+     *
+     * @return array
+     */
+    public function buildReactListAppConfig()
+    {
+        return [
+            "crudId"           => $this->crudId,
+            "basepath"         => $this->getRoutePrefix(),
+            "namespace"        => $this->namespace,
+            "model"            => $this->modelName,
+            "modelLabel"       => $this->getRecordLabel(),
+            "permissions"      => $this->getPermissionConfig(),
+            "disableSelection" => $this->disableSelection,
+            "controls"         => $this->buildReactListAppControlsConfig(),
+        ];
+    }
+
+    /**
+     * buildReactListAppControlsConfig builds the control definitions 
+     * for the react app.
+     */
+    protected function buildReactListAppControlsConfig()
+    {
+        $controls = [];
+        if ($this->canCreate) {
+            $controls[] = [
+                "label" => $this->getCreateButtonLabel(),
+                "feature" => "create"
+            ];
+        }
+        if ($this->canImport) {
+            $controls[] = [
+                "label" => "Import",
+                "feature" => "import"
+            ];
+        }
+        if ($this->canExport) {
+            $controls[] = [
+                "label" => "Export",
+                "feature" => "export",
+            ];
+        }
+        return $controls;
+    }
 }
 
