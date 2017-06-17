cd3cf0aace034001d23cf98b785c3f38ad81dd99
c9s <yoanlin93@gmail.com>
2017-06-17 13:02:10 +0800

    update webpack config

--- Assets/crud/webpack.config.js
+++ Assets/crud/webpack.config.js
@@ -1,6 +1,6 @@
-var path = require('path');
-var phifty = require('phifty');
-var webpack = require("phifty/node_modules/webpack");
+var path = require("path");
+var phifty = require("phifty");
+var webpack = require("webpack");
 var excludePaths = phifty.webpackExcludePaths();
 var aliases = phifty.assetAliases();
 
@@ -8,19 +8,28 @@ module.exports = {
   entry: __dirname + '/entry.js',
   output: { path: __dirname, filename: './bundle.js' },
   module: {
-    loaders: [{
-      test: /\.(js|jsx)$/,
-      loaders: ['babel-loader'],
-      exclude: excludePaths
-    }]
+    loaders: [
+      {
+        'test': /\.tsx?$/,
+        'loaders': ['ts-loader'],
+        'exclude': [/node_modules/,excludePaths]
+      },
+      {
+        'test': /\.jsx?$/,
+        'loaders': ['babel'],
+        'exclude': [/node_modules/,excludePaths]
+      }
+    ]
   },
+
   externals: {
     // don't bundle the 'react' npm package with our bundle.js
     // but get it from a global 'React' variable
     'react': 'React'
   },
+
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['', '.ts', '.tsx', '.js', '.jsx'],
     fallback: [ path.join(__dirname, "node_modules"), phifty.moduleDirectory],
     alias: aliases
   },
